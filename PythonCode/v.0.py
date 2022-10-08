a89eaa6e0f50dba066be6c61593565288d92378c
marcelatanaka-datascience <97251510+marcelatanaka-datascience@users.noreply.github.com>
2022-10-08 19:59:46 -0300

    Update and rename v.0 to v.0.py

---
+++ PythonCode/v.0.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dfAC = pd.read_csv('bweb_1t_AC_051020221321.csv', encoding = 'Latin 1', sep=';')
+dfAL = pd.read_csv('bweb_1t_AL_051020221321/bweb_1t_AL_051020221321.csv', encoding = 'Latin 1', sep=';')
+dfAM = pd.read_csv('bweb_1t_AM_051020221321/bweb_1t_AM_051020221321.csv', encoding = 'Latin 1', sep=';')
+dfAP = pd.read_csv('bweb_1t_AP_051020221321/bweb_1t_AP_051020221321.csv', encoding = 'Latin 1', sep=';')
+dfBA = pd.read_csv('bweb_1t_BA_051020221321/bweb_1t_BA_051020221321.csv', encoding = 'Latin 1', sep=';')
+dfCE = pd.read_csv('bweb_1t_CE_051020221321/bweb_1t_CE_051020221321.csv', encoding = 'Latin 1', sep=';')
